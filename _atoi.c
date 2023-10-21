a225914f7344def12e91174714b1b136af361a54
Professor-Masha <masha_e.maina@outlook.com>
2023-10-21 01:43:57 -0700

    simple shell

--- _atoi.c
+++ _atoi.c
@@ -23,10 +23,10 @@ int is_separator(char c, char separator)
 {
 	while (separator)
 	{
-	       	if (*separator++ == c)
-		       	return (1);
-       	}
-       	return (0);
+		if (*separator++ == c)
+			return (1);
+	}
+	return (0);
 }
 
 /**
@@ -38,9 +38,9 @@ int is_separator(char c, char separator)
 int is_alphabetic(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	       	return (1);
-       	else
-	       	return (0);
+		return (1);
+	else
+		return (0);
 }
 
 /**
@@ -52,27 +52,27 @@ int is_alphabetic(int c)
 int string_to_integer(char *s)
 {
 	int i, sign = 1, flag = 0, output;
-       	unsigned int result = 0;
+	unsigned int result = 0;
 
 	for (i = 0; s[i] != '\0' && flag != 2; i++)
-       	{
-	       	if (s[i] == '-')
-			sign *= -1;
-	       
-	if (s[i] >= '0' && s[i] <= '9')
 	{
-		flag = 1;
-	       	result *= 10;
-	       	result += (s[i] - '0');
-       	}
-       	else if (flag == 1)
-		flag = 2;
-       	}
-       
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
 	if (sign == -1)
 		output = -result;
-       	else
-	       	output = result;
-	
+	else
+		output = result;
+
 	return (output);
 }
